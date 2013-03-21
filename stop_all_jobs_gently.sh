fd0918dacd4188ee0dc7bb60602aa1a58005c1c0
Mike Kuiper <mkuiper@unimelb.edu.au>
2013-03-22 09:27:11 +1100

    First commit of MD workflow.

---
+++ stop_all_jobs_gently.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is a simple launch script to run: 
+# <main>/Scripts/Job_management_Scripts/stop_all_jobs_gently
+
+# This will stop all jobs in the job directories under ../MainJob_dir , by creating a 
+# pausejob flag.
+# Current running jobs will end on the next round.  
+
+# launch script: 
+
+Scripts/Job_management_Scripts/stop_all_jobs_gently 
+ 
+# 
---
+++ stop_all_jobs_immediately.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is a simple launch script to run: 
+# <main>/Scripts/Job_management_Scripts/stop_all_jobs_immediately
+
+# This will stop all jobs in the job directories under ../MainJob_dir ,-- immeditately! 
+#  no soft landing!  Current jobs will probbly loose data 
+# launch script: 
+
+Scripts/Job_management_Scripts/stop_all_jobs_immediately
+ 
+# 
